c927da0f13d7ad407d1004cd7405d8bca6bf4c53
hn410 <hn410@>
2021-05-11 13:16:00 +0900

    5 kadai-a finish

---
+++ 5/kadai-a/myexec.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[], char *envp[]){
+    pid_t pid;
+    if(argc < 2){
+        printf("Error: Too few arguments.");
+        exit(1);
+    }
+    if((pid = fork()) == 0){
+        char ** arg;
+        if(argc > 3) arg = &argv[2];
+        else arg = NULL;
+        execve(argv[1], arg, envp);
+        printf("Error: Failed to exec the program.");
+        exit(1);        
+    }else {
+        int status;
+        wait(&status);
+    }    
+    return 0;
+}
